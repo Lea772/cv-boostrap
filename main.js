cdd358a684c42f08b812586107575c6cbf9e4966
Lea772 <leabougarel@gmail.com>
2025-12-05 16:03:29 +0100

    message d'erreur

--- main.js
+++ main.js
@@ -2,3 +2,7 @@ function toggleResumeButton () {
   const resumeBtn = document.getElementById('resume-btn')
   resumeBtn.classList.toggle('d-none')
 }
+
+document.getElementById("monBouton").addEventListener("click", function() {
+    document.getElementById("message").textContent = "Message affiché !";
+});
